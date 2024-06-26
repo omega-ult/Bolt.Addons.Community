f80b11679a87a0e0fadd7773020c04db6908e56a
Kee <omega.ult@gmail.com>
2024-06-26 19:45:12 +0800

    add bound detect for between node

--- Runtime/Fundamentals/Nodes/Logic/Boolean/Between.cs
+++ Runtime/Fundamentals/Nodes/Logic/Boolean/Between.cs
@@ -1,15 +1,36 @@
 ﻿namespace Unity.VisualScripting.Community
-{   
+{
     /// <summary>
-     /// Returns whether the value is within the given range (inclusive).
-     /// </summary>
+    /// Returns whether the value is within the given range (inclusive).
+    /// </summary>
     [UnitCategory("Community\\Logic")]
     [RenamedFrom("Bolt.Addons.Community.Logic.Units.Between")]
     [RenamedFrom("Bolt.Addons.Community.Fundamentals.Between")]
     [TypeIcon(typeof(Unity.VisualScripting.And))]
     public class Between : Unit
     {
-        public Between() : base() { }
+        public Between() : base()
+        {
+        }
+
+        [SerializeAs(nameof(IncludeMin))] private bool _includeMin;
+        [SerializeAs(nameof(IncludeMin))] private bool _includeMax;
+
+        [DoNotSerialize]
+        [Inspectable, UnitHeaderInspectable("IncludeMin")]
+        public bool IncludeMin
+        {
+            set => _includeMin = value;
+            get => _includeMin;
+        }
+
+        [DoNotSerialize]
+        [Inspectable, UnitHeaderInspectable("IncludeMax")]
+        public bool IncludeMax
+        {
+            set => _includeMax = value;
+            get => _includeMax;
+        }
 
         /// <summary>
         /// The input value to check
@@ -33,7 +54,6 @@
         public ValueInput max { get; private set; }
 
 
-
         /// <summary>
         /// Whether the input is within the specified range.
         /// </summary>
@@ -46,7 +66,7 @@
             input = ValueInput<float>(nameof(input));
             min = ValueInput<float>(nameof(min), 0);
             max = ValueInput<float>(nameof(max), 0);
-            within = ValueOutput<bool>(nameof(within), (flow)=>GetWithin(flow));
+            within = ValueOutput<bool>(nameof(within), (flow) => GetWithin(flow));
 
             Requirement(input, within);
             Requirement(min, within);
@@ -56,9 +76,11 @@
         private bool GetWithin(Flow flow)
         {
             float inputValue = flow.GetValue<float>(input);
-
-            return (inputValue >= flow.GetValue<float>(min)) &&
-                    (inputValue <= flow.GetValue<float>(max));
+            var minVal = flow.GetValue<float>(min);
+            var maxVal = flow.GetValue<float>(max);
+            var boundMin = _includeMin ? inputValue >= minVal : inputValue > minVal;
+            var boundMax = _includeMax ? inputValue <= maxVal : inputValue < maxVal;
+            return boundMin && boundMax;
         }
     }
 }
